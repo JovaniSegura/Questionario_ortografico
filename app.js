c22a17671214c749eecc4fb887e3ca43934bfa6d
Jovani Segura <giovser7@gmail.com>
2023-10-23 21:24:41 -0500

    Ya esta en funcionamiento, se agrego todo lo principal

--- app.js
+++ app.js
@@ -1,20 +1,60 @@
-const fs = require('node:fs')
-
-console.clear();
+const fs = require('node:fs');
+const { imprimirInquirer } = require('./helpers/inquirer');
 
 const data = JSON.parse(fs.readFileSync('./data.json', { encoding: 'utf8' }))
 
-const random = Array.from({ length: 10 }, () => Math.floor(Math.random() * 50))
+// const random = Array.from({ length: 10 }, () => Math.floor(Math.random() * 50))
+const random = getRandomNonRepeatingNumbers(50, 10);
 
 let contador = 0
+let correctas = 0
 
-// console.log(data[random[0]]);
-const obtenerOpt = () => {
-    const {correct,incorrect} = data[random[0]]
-    const combinar = [correct,...incorrect]
-    combinar.sort(()=>Math.random()-0.5)
-    return combinar
-} 
+const main = async () => {
+    const obtenerOpt = () => {
+        const { correct, incorrect } = data[random[contador]]
+        const combinar = [correct, ...incorrect]
+        combinar.sort(() => Math.random() - 0.5)
+        return combinar
+    }
 
-// await imprimirInquirer(data[random[0]]);
-console.log(obtenerOpt());
+    const ejec = async () => {
+        console.clear()
+        console.log(`Escoga la palabra correcta \n`);
+        const impresion = await imprimirInquirer(obtenerOpt(),contador);
+        const palabraCorrecta = data[random[contador]].correct
+        if (impresion == palabraCorrecta) {
+            correctas++
+        }
+        contador++
+    }
+
+    while (contador < 10) {
+        await ejec()
+    }
+    console.clear();
+    console.log('Se acabo, usted tuvo ', correctas, ' Respuestas Correctas');
+    console.log(`Y tuvo ${10-correctas} Palabras incorrectas`);
+    console.log(`Su porcentaje de acierto fue: ${correctas/10*100}%`);
+}
+
+function getRandomNonRepeatingNumbers(range, count) {
+    if (range < count) {
+        throw new Error("No puedes generar más números únicos de los que hay en el rango.");
+    }
+
+    const randomNumbers = [];
+    for (let i = 0; i < range; i++) {
+        randomNumbers.push(i);
+    }
+
+    const result = [];
+    for (let i = 0; i < count; i++) {
+        const randomIndex = Math.floor(Math.random() * randomNumbers.length);
+        const randomNumber = randomNumbers.splice(randomIndex, 1)[0];
+        result.push(randomNumber);
+    }
+
+    return result;
+}
+
+main()
--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -1,5 +1,25 @@
 const inquirer = require('inquirer')
 
-imprimirInquirer = async (data) => {
-    const 
+imprimirInquirer = async (data = [], contador) => {
+    const choices = []
+    data.forEach((pal) => {
+        choices.push({
+            value: pal,
+            name: pal
+        })
+    })
+
+    const question = {
+        type: 'list',
+        message: `Palabra #${contador+1} de 10 \n`,
+        name: 'palabras',
+        choices
+    }
+
+    const { palabras } = await inquirer.prompt(question)
+    return palabras
+}
+
+module.exports = {
+    imprimirInquirer
 }
